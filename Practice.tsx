b95c6ce7c01855fab13e7a7e5512450fbe0495a4
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-11-01 17:46:42 +0100

    feat(Practice): display seconds counter

--- Practice.tsx
+++ Practice.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
 
 function Practice() {
+  const startTime = Date.now();
+
+  const [seconds, setSeconds] = React.useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const elapsedSeconds = Math.floor((Date.now() - startTime) / 1000);
+
+      if (elapsedSeconds > seconds) {
+        setSeconds(elapsedSeconds);
+      }
+    }, 200);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <View>
-      <Text>Practice</Text>
+      <Text>{seconds}</Text>
     </View>
   );
 }
