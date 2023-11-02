5ecbb732b0fd427826698bc83252ea76fb2f5c24
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-11-02 09:22:24 +0100

    refactor: create folder for screens

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 
 import {Navigation} from 'react-native-navigation';
 import App from './App';
-import Practice from './Practice';
-import PracticeSummary from './PracticeSummary';
+import Practice from './screens/Practice';
+import PracticeSummary from './screens/PracticeSummary';
 
 Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
 Navigation.registerComponent('com.myApp.Practice', () => Practice);
