f148ece34b14bdb52bc4ef56fecca3202de51bb0
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-11-02 09:34:22 +0100

    feat(PracticeSummary): layout

--- screens/PracticeSummary.tsx
+++ screens/PracticeSummary.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
-import {View, Text, Pressable} from 'react-native';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
 
 function PracticeSummary({entryTitle, duration}) {
   return (
-    <View>
-      <Text>{entryTitle}</Text>
-      <Text>{duration}</Text>
-      <Pressable>
-        <Text>Reset</Text>
-      </Pressable>
-      <Pressable>
-        <Text>Save entry</Text>
-      </Pressable>
+    <View style={styles.container}>
+      <View>
+        <Text>Well done!</Text>
+      </View>
+      <View>
+        <Text>Practice Summary</Text>
+        <Text>Entry title:</Text>
+        <Text>{entryTitle}</Text>
+        <Text>{duration}</Text>
+      </View>
+      <View>
+        <Pressable>
+          <Text>Reset</Text>
+        </Pressable>
+        <Pressable>
+          <Text>Save entry</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  header: {},
+  body: {},
+  footer: {},
+  button: {},
+  buttonLeft: {},
+  buttonRight: {},
+  buttonLabel: {},
+  entryTitle: {},
+  timeDisplay: {},
+});
 
 export default PracticeSummary;
