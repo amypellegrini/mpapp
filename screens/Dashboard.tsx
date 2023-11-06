8f657ff34384768930273748cb89189c3683b0f4
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-11-06 16:17:09 +0100

    fix: move practice history to practice journal

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {View, Text, StyleSheet, Pressable} from 'react-native';
 import {useRealm, useQuery} from '@realm/react';
-import RealmProviderWrapper, {PracticeEntry} from './RealmProviderWrapper';
+import RealmProviderWrapper, {
+  PracticeEntry,
+  PracticeEntrySummary,
+} from './RealmProviderWrapper';
 import Container from './components/container';
 import Title from './components/title';
 import Main from './components/main';
@@ -40,44 +43,16 @@ function formatDuration(seconds: number): string {
 
 function DashboardContent() {
   const realm = useRealm();
-  const entries = useQuery<PracticeEntry>('PracticeEntry');
+  const entries = useQuery<PracticeEntrySummary>('PracticeEntrySummary');
 
   const totalTime = entries.reduce((total, entry) => {
-    return total + entry.duration;
+    return total + entry.totalDuration;
   }, 0);
 
   return (
     <Container>
       <Main>
         <Title>Dashboard</Title>
-        {entries.length > 0 && (
-          <View style={styles.mb10}>
-            {entries.map(entry => (
-              <View style={styles.entryItem} key={entry._id.toString()}>
-                <View>
-                  <Text style={[styles.entryItemTitle, styles.entryItemText]}>
-                    {entry.title}
-                  </Text>
-                  <Text style={styles.entryItemText}>
-                    {entry.createdAt.toLocaleString()}
-                  </Text>
-                  <Text style={styles.entryItemText}>
-                    {formatDuration(entry.duration)}
-                  </Text>
-                </View>
-                <Pressable
-                  style={styles.deleteButton}
-                  onPress={() => {
-                    realm.write(() => {
-                      realm.delete(entry);
-                    });
-                  }}>
-                  <Text style={styles.deleteButtonText}>x</Text>
-                </Pressable>
-              </View>
-            ))}
-          </View>
-        )}
         <View>
           <Text style={styles.h4}>Total practice time:</Text>
           {totalTime === 0 && <Text>You haven't practiced anything yet!</Text>}
--- screens/PracticeJournal.tsx
+++ screens/PracticeJournal.tsx
@@ -4,12 +4,47 @@ import Container from './components/container';
 import Main from './components/main';
 import Title from './components/title';
 import RealmProviderWrapper, {
+  PracticeEntry,
   PracticeEntrySummary,
 } from './RealmProviderWrapper';
-import {useQuery} from '@realm/react';
-import {View, Text, StyleSheet} from 'react-native';
+import {useQuery, useRealm} from '@realm/react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
+
+function formatDuration(seconds: number): string {
+  if (seconds === 0) {
+    return '0s';
+  }
+
+  const time = {
+    hours: Math.floor(seconds / 3600),
+    minutes: Math.floor((seconds % 3600) / 60),
+    seconds: seconds % 60,
+  };
+
+  let duration = '';
+
+  if (time.hours) {
+    duration += `${time.hours} hour${time.hours > 1 ? 's' : ''} `;
+  }
+
+  if (time.minutes) {
+    duration += `${time.minutes} minute${time.minutes > 1 ? 's' : ''} `;
+  }
+
+  if (time.minutes && time.seconds) {
+    duration += 'and ';
+  }
+
+  if (time.seconds) {
+    duration += `${time.seconds} second${time.seconds > 1 ? 's' : ''}`;
+  }
+
+  return duration.trim();
+}
 
 function PracticeJournalContent() {
+  const realm = useRealm();
+  const entries = useQuery<PracticeEntry>('PracticeEntry');
   const entrySummaryList = useQuery<PracticeEntrySummary>(
     'PracticeEntrySummary',
   );
@@ -27,6 +62,36 @@ function PracticeJournalContent() {
             </Text>
           </View>
         ))}
+        {entries.length > 0 && (
+          <View style={styles.mb10}>
+            <Text style={styles.h6}>Practice history:</Text>
+            {entries.map(entry => (
+              <View style={styles.entryItem} key={entry._id.toString()}>
+                <View>
+                  <Text style={[styles.entryItemTitle, styles.entryItemText]}>
+                    {entry.title}
+                  </Text>
+                  <Text style={styles.entryItemText}>
+                    {entry.createdAt.toLocaleString()}
+                  </Text>
+                  <Text style={styles.entryItemText}>
+                    {formatDuration(entry.duration)}
+                  </Text>
+                </View>
+                {/* TODO: move this feature to EntrySummaryDetail */}
+                {/* <Pressable
+                  style={styles.deleteButton}
+                  onPress={() => {
+                    realm.write(() => {
+                      realm.delete(entry);
+                    });
+                  }}>
+                  <Text style={styles.deleteButtonText}>x</Text>
+                </Pressable> */}
+              </View>
+            ))}
+          </View>
+        )}
       </Main>
     </Container>
   );
@@ -47,6 +112,44 @@ const styles = StyleSheet.create({
   },
   mb10: {
     marginBottom: 10,
+  },
+  entryItem: {
+    borderRadius: 4,
+    backgroundColor: '#DFDFDF',
+    padding: 5,
+    paddingLeft: 10,
+    paddingBottom: 8,
+    marginBottom: 5,
+    borderWidth: 1,
+    borderColor: '#666666',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  deleteButton: {
+    backgroundColor: '#D11111',
+    borderColor: '#B11111',
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 5,
+    marginLeft: 5,
+    width: 30,
+    height: 30,
+  },
+  deleteButtonText: {
+    fontFamily: 'Arial',
+    textAlign: 'center',
+    color: '#FAFAFA',
+    fontWeight: '700',
+    fontSize: 20,
+    marginTop: -8,
+  },
+  entryItemTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  entryItemText: {
+    color: '#111111',
   },
 });
 
