a8529b59df76c7ec7ca2c4a51cb2c98c603b07b6
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-11-07 15:25:53 +0100

    feat(Welcome): button icons

--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,6 +1,11 @@
+import 'react-native-get-random-values';
+
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import Icon from 'react-native-vector-icons/Fontisto';
+import FontistoIcon from 'react-native-vector-icons/Fontisto';
+import FoundationIcon from 'react-native-vector-icons/Foundation';
+import IoniconsIcon from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Container from './components/container';
 import Title from './components/title';
@@ -15,12 +20,7 @@ const Welcome = ({componentId}) => {
         <Title>The Music Pratice App</Title>
         <Button
           title="Practice"
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            flexDirection: 'row-reverse',
-          }}
+          style={styles.iconButton}
           onPress={() => {
             Navigation.push(componentId, {
               component: {
@@ -28,14 +28,15 @@ const Welcome = ({componentId}) => {
               },
             });
           }}>
-          <Icon
+          <FontistoIcon
             name="music-note"
-            style={{marginLeft: 10, marginRight: 15}}
+            style={styles.icon}
             size={30}
             color="#333333"
           />
         </Button>
         <Button
+          style={styles.iconButton}
           title="Dashboard"
           onPress={() => {
             Navigation.push(componentId, {
@@ -43,9 +44,16 @@ const Welcome = ({componentId}) => {
                 name: 'com.myApp.Dashboard',
               },
             });
-          }}
-        />
+          }}>
+          <IoniconsIcon
+            style={styles.icon}
+            name="stats-chart"
+            color="#333333"
+            size={26}
+          />
+        </Button>
         <Button
+          style={styles.iconButton}
           title="Goals"
           onPress={() => {
             Navigation.push(componentId, {
@@ -53,10 +61,17 @@ const Welcome = ({componentId}) => {
                 name: 'com.myApp.Goals',
               },
             });
-          }}
-        />
+          }}>
+          <FoundationIcon
+            style={styles.icon}
+            name="target"
+            color="#333333"
+            size={32}
+          />
+        </Button>
 
         <Button
+          style={styles.iconButton}
           title="Practice journal"
           onPress={() => {
             Navigation.push(componentId, {
@@ -64,13 +79,33 @@ const Welcome = ({componentId}) => {
                 name: 'com.myApp.PracticeJournal',
               },
             });
-          }}
-        />
+          }}>
+          <MaterialCommunityIcon
+            style={styles.icon}
+            // name="book-open-page-variant"
+            // name="file-document-edit-outline"
+            name="book-music-outline"
+            color="#333333"
+            size={30}
+          />
+        </Button>
       </Main>
     </Container>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  icon: {
+    marginLeft: 10,
+    marginRight: 10,
+    width: 30,
+  },
+  iconButton: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    flexDirection: 'row-reverse',
+  },
+});
 
 export default Welcome;
