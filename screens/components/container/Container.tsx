3eabee76e5b41c8197b56e7878af4a208736b960
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-11-16 08:52:50 +0100

    fix(Container): props type annotation

--- screens/components/container/Container.tsx
+++ screens/components/container/Container.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
-const Container = ({style = {}, children}) => {
-  const stylesArray = Array.isArray(style) ? style : [style];
-  const resolvedStyles = stylesArray.map(style =>
-    typeof style === 'function' ? style({pressed: false}) : style,
-  );
+interface ContainerProps extends React.ComponentProps<typeof View> {}
 
-  return <View style={[styles.container, ...resolvedStyles]}>{children}</View>;
+const Container = ({style, children}: ContainerProps) => {
+  const stylesArray = Array.isArray(style) ? style : [style];
+
+  return <View style={[styles.container, ...stylesArray]}>{children}</View>;
 };
 
 const styles = StyleSheet.create({
