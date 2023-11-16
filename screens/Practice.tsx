5f51ddfa28fc97b9567aa3a64c5a74bfbef87350
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-11-16 09:06:47 +0100

    feat(Practice): display BPM

--- screens/Practice.tsx
+++ screens/Practice.tsx
@@ -1,15 +1,21 @@
 import React, {useEffect} from 'react';
 
 import {View, Text, StyleSheet, useColorScheme} from 'react-native';
-import {Navigation} from 'react-native-navigation';
+import {Navigation, NavigationProps} from 'react-native-navigation';
 
 import Container from './components/container';
 import {ButtonLeft, ButtonRight} from './components/button/Button';
 import Title from './components/title';
 import commonStyles from './components/commonStyles';
 import formatTime from './components/utils/formatTime';
+import H2 from './components/Typography/H2';
 
-function Practice({entryTitle, componentId}) {
+interface PracticeProps extends NavigationProps {
+  entryTitle: string;
+  bpm?: number;
+}
+
+function Practice({entryTitle, bpm, componentId}: PracticeProps) {
   const isDarkMode = useColorScheme() === 'dark';
   const startTime = Date.now();
 
@@ -31,7 +37,10 @@ function Practice({entryTitle, componentId}) {
 
   return (
     <Container>
-      <Title style={commonStyles.textCenter}>{entryTitle}</Title>
+      <View style={commonStyles.pt10}>
+        <Title style={commonStyles.textCenter}>{entryTitle}</Title>
+        {bpm && <H2 style={commonStyles.textCenter}>{bpm} bpm</H2>}
+      </View>
       <View style={styles.body}>
         <Text
           style={[
