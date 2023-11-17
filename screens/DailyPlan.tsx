988c6571eb8e5ac1e1295baa06417d6e6470f18d
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-11-17 16:40:46 +0100

    feat(PracticeSchedule): display main menu

--- screens/DailyPlan.tsx
+++ screens/DailyPlan.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Container from './components/container';
 import Main from './components/main';
 import Title from './components/title';
+import Menu from './components/menu';
+import {NavigationProps} from 'react-native-navigation';
 
-function PracticeSchedule() {
+function PracticeSchedule({componentId}: NavigationProps) {
   return (
     <Container>
       <Main>
         <Title>Daily plan</Title>
       </Main>
+      <Menu componentId={componentId} />
     </Container>
   );
 }
